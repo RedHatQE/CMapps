c44289c5cfccf6f5c63e807fccd81b399b2ed150
gshefer <gshefer@redhat.com>
2018-02-07 15:23:46 +0200

    Updated stresser

--- apps/stresser/deploy.sh
+++ apps/stresser/deploy.sh
@@ -1,5 +1,5 @@
 oc new-project test
 oc create -f stresser.yaml
-oc new-app --template=stresser-template --param=PROJECT_NAME=test --param=REPLICAS=5
+oc new-app --template=stresser-template --param=PROJECT_NAME=test --param=REPLICAS=3
+sleep 10
 oc start-build stresser
-oc deploy stresser --latest
